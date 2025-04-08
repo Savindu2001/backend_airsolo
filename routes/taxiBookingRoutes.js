19b1729fc17ac8fac73f75c086949f155efadf50
Savindu2001 <76149334+Savindu2001@users.noreply.github.com>
2025-04-09 00:07:27 +0530

    update taxi booking

--- routes/taxiBookingRoutes.js
+++ routes/taxiBookingRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { bookingTaxi, updateBookingStatus } = require('../controllers/taxiBookingController');
+const { bookingTaxi, updateBookingStatus ,getAvailableSharedBookings } = require('../controllers/taxiBookingController');
 
 // Route to book a taxi
 router.post('/', bookingTaxi);
 
 // Route to update booking status
 router.patch('/update-booking/:bookingId', updateBookingStatus);
+
+// Get Shared Booking
+//router.get('/shared-available', getAvailableSharedBookings)
 
 module.exports = router;
