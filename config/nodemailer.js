6d50563553d96fb960cde33ac348171453b4a6f6
Savindu2001 <76149334+Savindu2001@users.noreply.github.com>
2025-04-09 04:41:38 +0530

    node mailer smtp fixed and email sent

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -3,10 +3,10 @@ const nodemailer = require('nodemailer');
 
 // Create a transporter object
 const transporter = nodemailer.createTransport({
-    // host: process.env.EMAIL_HOST,
-    // port: process.env.EMAIL_PORT,
-    // secure: true,
-    service: 'gmail', // Use your email service provider
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: true,
+    //service: 'gmail', 
     auth: {
         user: process.env.EMAIL_USER, 
         pass: process.env.EMAIL_PASS, 
