9c4d8d41e2977c1f6f0476584ef31038080cc02f
Savindu2001 <76149334+Savindu2001@users.noreply.github.com>
2025-04-09 10:57:54 +0530

    login routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,8 +4,10 @@ const userController = require('../controllers/userController');
 const { uploadProfilePhoto } = require('../middlewares/uploadProfilePhoto'); 
 const { authenticateJWT } = require("../middleware/auth");
 
+
 // Define your routes
 router.post('/register', userController.createUser); // Create a new user
+router.post('/login', userController.loginUser);
 router.get('/', authenticateJWT, userController.getAllUsers); // Get all users (protected route)
 router.get('/:id', authenticateJWT, userController.getUserById); // Get user by ID (protected route)
 router.put('/:id', authenticateJWT, userController.updateUser); // Update user (protected route)
