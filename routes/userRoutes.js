22ace7089b09a4c0110634f21456f39a6694b3f5
Savindu2001 <76149334+Savindu2001@users.noreply.github.com>
2025-04-13 12:11:15 +0530

    remove jwt for verify email

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,7 +10,7 @@ const checkRole = require('../middlewares/checkRole');
 
 router.post('/register', userController.createUser); 
 router.post('/login', userController.loginUser);
-router.get('/', authenticateJWT, checkRole(['traveler']), userController.getAllUsers); 
+router.get('/', authenticateJWT, checkRole(['admin','staff']), userController.getAllUsers); 
 router.get('/:id', authenticateJWT, checkRole(['admin']),  userController.getUserById); 
 router.put('/:id', authenticateJWT, checkRole(['admin']),  userController.updateUser); // Update user (protected route)
 router.delete('/:id', authenticateJWT , checkRole(['admin']),  userController.deleteUser); // Delete user (protected route)
@@ -23,7 +23,7 @@ router.delete('/:id/profile-photo', authenticateJWT, userController.deleteProfil
 
 // --- --- FireBase routes
 router.post('/forgot-password', userController.forgotPassword); // No auth required
-router.post('/send-verification-email', authenticateJWT, emailController.sendEmailVerification); // Requires auth
+router.post('/send-verification-email',emailController.sendEmailVerification); // Requires auth authenticateJWT, 
 
 
 
